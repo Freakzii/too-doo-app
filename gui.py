786ffefb61b94f2a7f7c25047c586d0e1ac41e78
jamie <jamietaylorbagnall@hotmail.co.uk>
2024-10-03 12:09:45 +0100

    start of gui

--- gui.py
+++ gui.py
@@ -5,8 +5,8 @@ import FreeSimpleGUI as sg
 
 labe = sg.Text("type a to do")
 input_box = sg.InputText(tooltip="enter todo")
-add_button = sg.Button
+add_button = sg.Button("Add")
 
-window  = sg.Window('My to-do-app',layout=[[labe, input_box]])
+window  = sg.Window('My to-do-app',layout=[[labe], [input_box,add_button]])
 window.read()
 window.close()
